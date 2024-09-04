1fff5fd18abfcbd35756658f205c4210169a60df
Gilmar Junior <rochagmjunior@gmail.com>
2024-09-04 17:56:01 -0300

    Novas alterações

---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    deuteronomio = {
+        titulo : "Deuteronômio",
+        descricao : "O livro de Deuteronômio é o quinto e último livro da Torá, a lei judaica. Ele se destaca como uma espécie de revisão e preparação do povo de Israel para a entrada na Terra Prometida, após décadas de peregrinação pelo deserto.",
+        link : "https://www.bibliaonline.com.br/acf/dt",
+        autor : "Moisés",
+        dataProvavel : "1410 a.C.",
+        curiosidades : {
+            origemNome : "- Seu nome vem do grego Δευτερονόμιον, \"Deuteronómion\", \"Segunda lei\"; em hebraico: דְּבָרִים, Devārīm, \"palavras [ditas]\" - o título em hebraico é derivado do primeiro versículo, \"Eleh ha-devarim\", \"Estas são as palavras que Moisés falou...\" (Dt 1:1);",
+
+            objecoesAltaCritica : "- Apesar das acusações da alta crítica remeterem sua autoria ao tempo do rei Josias (640 a.C. - 609 a.C.), algumas objeções simples as colocam em cheque: 1. A tradição judaica sempre atribuiu a autoria do Pentateuco em geral (incluindo Deuteronômio) a Moisés, isso é visto inclusive no próprio Novo Testamento que foi escrito no primeiro século: Mt 19:7-8 / Dt 24:1-4 - o próprio Senhor Jesus confirma isso; 2. Linguagem diferente do período monárquico, como um hebraico mais arcaico; 3. Referências a cidades, nações e reis não relevantes para aquele período (nem se fala sobre Jerusalém); 4. Citações do profeta Isaías (1 século antes do rei Josias): Is 1:17 / Dt 15:7-11, Is 4:5 / Ex 13:21-22 (Deuteronômio está ligado ao livro de Êxodo); etc."
+        } 
+    },
+    romanos = {
+        titulo : "Romanos",
+        descricao : "A carta aos Romanos é um dos escritos mais importantes do apóstolo Paulo. Nela, ele explora temas como a justificação pela fé, a lei e o evangelho, e a esperança da glória futura.",
+        link : "https://www.bibliaonline.com.br/acf/rm",
+        autor : "Paulo",
+        dataProvavel : "57 d.C.",
+        curiosidades : {
+            maiorEpistola : "É a maior epístola do Novo Testamento;",
+            conversaoCristaosFamosos : "Essa carta levou cristãos famosos, como Agostinho e Lutero à conversão."
+        }
+    },
+    evangelhoJoao = {
+        titulo : "Evangelho de João",
+        descricao : "O Evangelho de João apresenta um Jesus Cristo diferente dos outros evangelhos sinóticos. Ele o revela como o Logos, o Verbo de Deus, e enfatiza sua natureza divina e sua relação com o Pai.",
+        link : "https://www.bibliaonline.com.br/acf/jo",
+        autor : "João",
+        dataProvavel : "80 - 85 d.C.",
+        curiosidades : {
+            logos : "O \"Logos\" era visto pelos filósofos gregos como a força que dava ordem e sentido ao universo. Ao identificar Jesus como o Logos, João estava afirmando que Jesus é a razão última de todas as coisas, a fonte de toda a ordem e significado;",
+            gnosticos : "O Evangelho de João foi escrito em um contexto marcado pelo surgimento de heresias como o gnosticismo. Enquanto os gnósticos viam Jesus como um mero mestre espiritual, João afirma categoricamente que Jesus é o próprio Deus encarnado."
+        }
+    }
+];
+
